a27931ab9e0bb2eef98a49d7268f93f85c6b7754
Raymond McDonald <contact@wreckingballpublishing.com>
2024-07-03 15:00:16 -0500

    Add "parrot" endpoint to example server and demonstrate its functionality; run cargo fmt

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,7 @@
-use std::net::SocketAddr;
 use hyper::server::conn::Http;
 use hyper::service::service_fn;
 use hyper::{Body, Method, Request, Response, StatusCode};
+use std::net::SocketAddr;
 use tokio::net::TcpListener;
 
 /// This is our service handler. It receives a Request, routes on its
@@ -23,6 +23,16 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, hyper::Err
             Ok(Response::new(Body::from(reversed_body)))
         }
 
+        // Parrot the body back to the client.
+        (&Method::POST, "/parrot") => {
+            let body_bytes = hyper::body::to_bytes(req).await?;
+            let body_string = String::from_utf8(body_bytes.to_vec()).unwrap();
+
+            let mut parroted_string = String::from("You said: ");
+            parroted_string.push_str(&body_string);
+            Ok(Response::new(Body::from(parroted_string)))
+        }
+
         // Return the 404 Not Found for other routes.
         _ => {
             let mut not_found = Response::default();
@@ -42,7 +52,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         let (stream, _) = listener.accept().await?;
 
         tokio::task::spawn(async move {
-            if let Err(err) = Http::new().serve_connection(stream, service_fn(handle_request)).await {
+            if let Err(err) = Http::new()
+                .serve_connection(stream, service_fn(handle_request))
+                .await
+            {
                 println!("Error serving connection: {:?}", err);
             }
         });
